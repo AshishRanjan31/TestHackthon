421022f75f80fafb8e513ef96dc11cc0254a4a62
Ranjan <aranjan@paychex.com>
2019-09-17 00:50:45 +0530

    Latest code merged

--- assets/js/career-portal.js
+++ assets/js/career-portal.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     $("#jobSearchSubmit").click(function() {
       alert("Hello, world!");
+      var dept = $( "#jobSearch option:selected" ).text();
+      var jobLocation = $( "#locationSearch option:selected" ).text();
+      var jobtype = $( "#jobTypeSearch option:selected" ).text();
+      /*searchResult */
+
+      var txt1 = "<div class='row show-grid'><div class='col-xs-6 col-md-4'>" + dept;
+      var txt2 = "</div><div class='col-xs-6 col-md-4'>" +jobLocation;
+      var txt3 =  "</div><div class='col-xs-6 col-md-4 job-type'>" +jobtype+"</div></div>";
+
+      $('#searchResult').append(txt1 +txt2+ txt3)
+        
+       
     });
   });
